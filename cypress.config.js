b9c4a65e4666b3a44aa5ce88404e78b277e60652
Gdhaiya <gaurav@veersatech.com>
2023-03-11 16:36:06 +0530

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "k8npa2",
-  CYPRESS_RECORD_KEY: "46575950-3ff6-41f1-9942-7e6a4a2f2231",
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
